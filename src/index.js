c22b480f0abfa8610cb4e398b407169fea449a73
rovamad <rojasmarcos@outlook.com>
2019-12-22 20:04:26 -0300

    Practica creando proyecto React usando create-react-app y primer componente funcional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function MyInfo() {
+    return (
+        <div>
+            <h1>Marcos Rojas</h1>
+            <p>Information Engineer</p>
+            <ol>
+                <li>Cuyagua</li>
+                <li>Río de Janeiro</li>
+                <li>Cancun</li>
+            </ol>
+        </div>)
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<MyInfo />, document.getElementById('root'));
